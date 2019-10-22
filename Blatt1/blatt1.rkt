1b7f89e03bb82735429b3dc737e2326867c9e1b5
Jan-Thomas Riemenschneider <dajanto.1337@hotmail.de>
2019-10-22 22:04:45 +0200

    TODOs 1.1

--- Blatt1/blatt1.rkt
+++ Blatt1/blatt1.rkt
@@ -3,20 +3,20 @@
 ; 1.1 Bogenmaß und Grad
 (define (degrees->radians degrees)
   ; Gradzahlen -> Bogenmaß
-  ; TODO
+  ; TODO 1° × π/180 = 0,01745 rad
   degrees
   )
 
 (define (radians->degrees radians)
   ; Bogenmaß -> Gradzahl
-  ; TODO
+  ; TODO 1 rad × 180/π = 57,296°
   radians
   
   )
 
 ; 1.2 Umkehrfunktion
 (define (my-acos cos)
- ; TODO
  ; atan: tangens t -> winkel alpha
+ ; TODO cos to tan und atan to alpha
   cos
   )
