6574ea25ff3411dee7fa8aef2c24755971936e9e
Jan-Thomas Riemenschneider <dajanto.1337@hotmail.de>
2019-10-22 15:41:33 +0200

    1.1 und 1.2 Start

--- Blatt1/blatt1.rkt
+++ Blatt1/blatt1.rkt
@@ -1,1 +1,22 @@
 #lang racket
+
+; 1.1 Bogenmaß und Grad
+(define (degrees->radians degrees)
+  ; Gradzahlen -> Bogenmaß
+  ; TODO
+  degrees
+  )
+
+(define (radians->degrees radians)
+  ; Bogenmaß -> Gradzahl
+  ; TODO
+  radians
+  
+  )
+
+; 1.2 Umkehrfunktion
+(define (my-acos cos)
+ ; TODO
+ ; atan: tangens t -> winkel alpha
+  cos
+  )
