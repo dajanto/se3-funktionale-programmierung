140d2fb6eec4cdf71f6376b3d0fc0761cc602f70
Jan-Thomas Riemenschneider <dajanto.1337@hotmail.de>
2019-10-29 15:06:25 +0100

    1.3 und TODOs

--- Blatt1/blatt1.rkt
+++ Blatt1/blatt1.rkt
@@ -12,8 +12,31 @@
   )
 
 ; 1.2 Umkehrfunktion
-(define (my-acos x)
-  (define y (degrees->radians x)) 
-  (define newTan (/ (sin y) (cos y)))
-  ;(radians->degrees (atan newTan))
+; TODO Nicht alle Results stimmen
+(define (my-acos cosinus)
+  ; cosinus (degrees) zu radians
+  (define cosinusRadians (degrees->radians cosinus)) 
+  ; tangens berechnen
+  (define tangensRadians (/ (sin cosinusRadians) (cos cosinusRadians)))
+  ; tangens an atan übergeben und somit acos erhalten
+  (atan tangensRadians)
   )
+
+; 1.3 Kilometer und Seemeilen
+(define (nm->km nm)
+  (define km (* 1.852 nm))
+  km
+  )
+
+; 2.1 Großkreisentfernung
+(define (distanzAB a b)
+  a
+  )
+
+; Oslo->Hongkong
+; San Francisco_>Honolulu
+; Osterinsel->Lima
+
+; 2.2 Anfangskurs
+
+; 2.3 Himmelsrichtungen
