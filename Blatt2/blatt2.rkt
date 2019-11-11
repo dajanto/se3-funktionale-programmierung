7911d36f1d69809582b9a37f48261a949017d347
Jan-Thomas Riemenschneider <dajanto.1337@hotmail.de>
2019-11-11 17:04:00 +0100

    Start Aufgabe 2

--- Blatt2/blatt2.rkt
+++ Blatt2/blatt2.rkt
@@ -3,11 +3,27 @@
 (define Hund wuff)
 (define Wolf 'wuff)
 
-(define (welcherNameGiltWo PersonaA PersonB)
+(define (welcherNameGiltWo PersonA PersonB)
   (let ((PersonA 'Zaphod)
-      (PersonC PersonaA))
+        (PersonC PersonA))
     PersonC))
 
 (define xs1 '(0 2 3 wuff Hund))
 (define xs3 (list wuff Hund))
 (define xs2 (cons Hund wuff))
+
+; Aufgabe 1
+; Siehe .txt
+
+; Aufgabe 2
+
+; 2.1
+
+; 2.2
+
+; 2.3
+
+; 2.4
+
+; Aufgabe 3
+
