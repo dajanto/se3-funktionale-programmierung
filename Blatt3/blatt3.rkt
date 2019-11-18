42982b38d51f12a3bc0d85891dff8dcdba33f931
Jan-Thomas Riemenschneider <dajanto.1337@hotmail.de>
2019-11-18 19:58:04 +0100

    start

---
+++ Blatt3/blatt3.rkt
@@ -0,0 +1,17 @@
+#lang racket
+
+; Aufgabe 1 Internationale Buchstabiertafel 
+
+; 1.1 Datenstruktur 
+
+; 1.2 Codierungsfunktion
+
+; 1.4 Buchstabieren 
+
+; Aufgabe 2 Internationales Flaggenalphabet
+
+; 2.1 Datenstruktur 
+
+; 2.2 Codierungsfunktion
+
+; 2.3 Buchstabieren 
