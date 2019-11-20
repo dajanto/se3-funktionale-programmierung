55f96577580893d1484f0f55b641c408696118ff
Alasdair Grimm <5grimm@informatik.uni-hamburg.de>
2019-11-20 13:50:20 +0100

    Blatt3 bearbeitet

--- Blatt3/blatt3.rkt
+++ Blatt3/blatt3.rkt
@@ -1,4 +1,5 @@
 #lang racket
+(require se3-bib/flaggen-module)
 
 ; Aufgabe 1 Internationale Buchstabiertafel 
 
@@ -47,11 +48,53 @@
            (buchstabieren (list->string(cdr liste)))])
   )
 
+;Aufgabe 2 Internationales Flaggenalphabet
 
-; Aufgabe 2 Internationales Flaggenalphabet
-
-; 2.1 Datenstruktur 
-
+; 2.1 Datenstruktur
+(define Flaggenalfabet(hash
+         "A" A
+         "B" B
+         "C" C
+         "D" D
+         "E" E
+         "F" F
+         "G" G
+         "H" H
+         "I" I
+         "J" J
+         "K" K
+         "L" L
+         "M" M
+         "N" N
+         "O" O
+         "P" P
+         "Q" Q
+         "R" R
+         "S" S
+         "T" T
+         "U" U
+         "V" V
+         "W" W
+         "X" X
+         "Y" Y
+         "Z" Z
+         "0" Z0
+         "1" Z1
+         "2" Z2
+         "3" Z3
+         "4" Z4
+         "5" Z5
+         "6" Z6
+         "7" Z7
+         "8" Z8
+         "9" Z9)
+  )
 ; 2.2 Codierungsfunktion
 
-; 2.3 Buchstabieren 
+(define (FlaggenAbbilden value)
+  (hash-ref Flaggenalfabet value))
+
+; 2.3 Buchstabieren
+
+(define (buchstabieren2 str)
+  (for-each Codierungsfunktion (string->list str)))
