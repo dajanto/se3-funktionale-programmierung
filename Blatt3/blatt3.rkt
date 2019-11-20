ae17a8adfb29ec880d2fd393cb83af8af81a54d6
Jan-Thomas Riemenschneider <dajanto.1337@hotmail.de>
2019-11-20 14:05:30 +0100

    Alphabet

--- Blatt3/blatt3.rkt
+++ Blatt3/blatt3.rkt
@@ -1,6 +1,8 @@
 #lang racket
 
-; Aufgabe 1 Internationale Buchstabiertafel 
+; Aufgabe 1 Internationale Buchstabiertafel
+
+(require se3-bib/flaggen-module)
 
 ; 1.1 Datenstruktur 
 (define buchstabiertafel (hash
@@ -8,6 +10,38 @@
                           #\B 'Bravo	
                           #\C 'Charlie	
                           #\D 'Delta
+                          #\E 'Echo
+                          #\F 'Foxtrott
+                          #\G 'Golf
+                          #\H 'Hotel
+                          #\I 'India
+                          #\J 'Juliett
+                          #\K 'Kilo
+                          #\L 'Lima
+                          #\M 'Mike
+                          #\N 'November
+                          #\O 'Oscar
+                          #\P 'Papa
+                          #\Q 'Quebec
+                          #\R 'Romeo
+                          #\S 'Sierra
+                          #\T 'Tango
+                          #\U 'Uniform
+                          #\V 'Viktor
+                          #\W 'Whiskey
+                          #\X 'X-ray
+                          #\Y 'Yankee
+                          #\Z 'Zulu
+                          #\0 'Nadazero
+                          #\1 'Unaone
+                          #\2 'Bissotwo
+                          #\3 'Terrathree
+                          #\4 'Kartefour
+                          #\5 'Pandafive
+                          #\6 'Soxisix
+                          #\7 'Setteseven
+                          #\8 'Oktoeight
+                          #\9 'Novenine
                           #\, 'Decimal
                           #\. 'Stop
                           ))
