c9ce8fa5cf4492f39f5b88455a8fdb98bcc22938
Alasdair Grimm <5grimm@informatik.uni-hamburg.de>
2019-11-23 13:31:08 +0100

    Aufgabe 1 Blatt4 bearbeitet und neue Blätter hinzugefügt

---
+++ Blatt4/Blatt4.rkt
@@ -0,0 +1,21 @@
+#lang racket
+
+;;;;; Aufgabe 1
+
+;;; Eingaben:
+
+; (max(min 2(- 2 5))0)
+
+; '(+ (- 2 13)2)
+
+; (cadr '(Alle Jahre wieder))
+
+; (cddr '(kommt (das Weihnachtsfest)))
+
+; (cons 'Listen '(ganz einfach und))
+
+; (cons 'Paare 'auch)
+
+; (equal? (list 'Racket 'Prolog 'Java) '(Racket Prolog Java))
+
+; (eq? (list 'Racket 'Prolog 'Java) (cons 'Racket '(Prolog Java)))
