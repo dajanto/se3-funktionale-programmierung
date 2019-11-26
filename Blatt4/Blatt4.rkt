2e24f2cb6242fc79a5272f44261a08e9d72b89c5
Jan-Thomas Riemenschneider <dajanto.1337@hotmail.de>
2019-11-26 21:52:14 +0100

    2.1 finish

--- Blatt4/Blatt4.rkt
+++ Blatt4/Blatt4.rkt
@@ -1,21 +1,43 @@
 #lang racket
 
-;;;;; Aufgabe 1
+; Aufgabe 1 Auswertung von Ausdrücken
 
-;;; Eingaben:
-
+; Eingaben:
 ; (max(min 2(- 2 5))0)
-
 ; '(+ (- 2 13)2)
-
 ; (cadr '(Alle Jahre wieder))
-
 ; (cddr '(kommt (das Weihnachtsfest)))
-
 ; (cons 'Listen '(ganz einfach und))
-
 ; (cons 'Paare 'auch)
-
 ; (equal? (list 'Racket 'Prolog 'Java) '(Racket Prolog Java))
-
 ; (eq? (list 'Racket 'Prolog 'Java) (cons 'Racket '(Prolog Java)))
+
+
+; Aufgabe 2 Textgenerierung
+
+; 2.1
+; Backus-Naur-Form
+
+;1 <Überschrift> ::= <Notzeichen> <Notzeichen> <Notzeichen> "DELTA ECHO" <Schiffsname> <Schiffsname> <Schiffsname> <Rufzeichen> <NotzeichenEnde>
+;2 <Standortangabe> ::= [a-zA-Z]
+;3 <Art des Unfalls> ::= [a-zA-Z]
+;4 <Angaben zur Hilfeleistung> ::= [a-zA-Z]
+;5 <Peilzeichen> ::= (Sendetaste für 10-15 Sekunden drücken)
+;6 <Unterschrift> ::= <Schiffsname> <RufzeichenBuchstabiert>
+;7 <OVER> ::= "OVER"
+
+; Auflösungen
+; <Notzeichen> ::= "MAYDAY"
+; <NotzeichenEnde> ::= "MAYDAY" <Schiffsname> <SchiffsnameBuchstabiert> <RufzeichenBuchstabiert>
+; <Schiffsname> ::= [a-zA-Z]
+; <SchiffsnameBuchstabiert> ::= [Alpha,Bravo,Charlie,...,Zulu]
+; <Rufzeichen> ::= [Alpha,Bravo,Charlie,...,Zulu] [Alpha,Bravo,Charlie,...,Zulu] [Alpha,Bravo,Charlie,...,Zulu] [Alpha,Bravo,Charlie,...,Zulu]
+; <RufzeichenBuchstabiert> ::= [Alpha,Bravo,Charlie,...,Zulu]
+
+; 2.2
+
+; 2.3
+
+
+
+; Aufgabe 3 Funktionen vs. Spezialformen
