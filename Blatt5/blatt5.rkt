1758618d5d04406ae57ff5b771a89fe87a6fecad
Jan-Thomas Riemenschneider <dajanto.1337@hotmail.de>
2019-12-02 13:03:58 +0100

    Blatt5 start

--- Blatt5/blatt5.rkt
+++ Blatt5/blatt5.rkt
@@ -1,6 +1,29 @@
 #lang racket
 
+(require se3-bib/butterfly-module)
+
+
 ; Aufgabe 1
+
+; Analyse und Grobentwurf
+
+; Ziel
+; Bei Angabe von zwei dominanten Merkmalen x Kinder erzeugen und beschreiben
+; Rezessive Merkmale werden zufällig, aber in Abhängigkeit der Dominanzregeln ausgewählt 
+; Anzeigen mit (show-butterfly)
+
+; Benötigte Funktionen
+; Funktion zur Generierung der Kinder (mendeln)
+; Funktion zur Darstellung 
+; Testaufrufe mit vorhandenen Funktionen
+
+; Datenstrukturen
+; 
+
+; Tests
+(show-butterfly 'red 'dots 'straight 'hexagon)
+(show-butterfly 'yellow 'star 'straight 'ellipse)
+(show-butterfly 'blue 'star 'curly 'rhomb)
 
 
 ; Aufgabe 2
