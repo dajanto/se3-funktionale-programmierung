478ffb00cc7fee6a8f4bb72fe72d949b42a2abe5
Jan-Thomas Riemenschneider <dajanto.1337@hotmail.de>
2019-12-02 13:36:26 +0100

    blatt5 planung

--- Blatt5/blatt5.rkt
+++ Blatt5/blatt5.rkt
@@ -5,6 +5,17 @@
 
 ; Aufgabe 1
 
+; Schmetterling -> Ein dominantes und ein rezessives Merkmal
+
+; Dominanzregeln
+; Sterne > Punkte, Streifen
+; rot > grün, blau, gelb
+; grün > gelb, blau
+; blau > gelb
+; Gekrümmt > gerade, geschweift
+; Gerade > geschweift
+; Elliptisch > hexagonal, rhombisch
+; Hexagonal > rhombisch
 ; Analyse und Grobentwurf
 
 ; Ziel
@@ -13,17 +24,35 @@
 ; Anzeigen mit (show-butterfly)
 
 ; Benötigte Funktionen
-; Funktion zur Generierung der Kinder (mendeln)
+; Funktion zur Generierung der Kinder ("mendeln")
 ; Funktion zur Darstellung 
+; Funktion zur Dominanzüberprüfung eines Merkmals
 ; Testaufrufe mit vorhandenen Funktionen
 
 ; Datenstrukturen
-; 
+; Speicherung von Merkmal-Kategorien (Musterung, Flügelfarbe, Fühlerform, Flügelform)
 
-; Tests
+; Implementation und Erprobung
+
+(define musterung (list 'sterne 'punkte 'streifen)
+)
+
+(define fluegelfarbe (list 'red 'gelb 'gruen 'blau)
+)
+
+(define fuehlerform (list 'gerade 'gekruemmt 'geschweift)
+)
+
+(define fluegelform (list 'rhombisch 'elliptisch 'hexagonal)
+)
+
+(define (mendeln merkmalA merkmalB kinderanzahl)
+  merkmalA
+  )
+
+; Testaufrufe
 (show-butterfly 'red 'dots 'straight 'hexagon)
 (show-butterfly 'yellow 'star 'straight 'ellipse)
 (show-butterfly 'blue 'star 'curly 'rhomb)
-
 
 ; Aufgabe 2
