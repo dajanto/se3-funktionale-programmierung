8d415962c9347aa5f0ab4fbed3ee24997b01c1ef
Jan-Thomas Riemenschneider <dajanto.1337@hotmail.de>
2019-12-11 17:00:28 +0100

    Aufgabe 1 finish

--- Blatt7/blatt7.rkt
+++ Blatt7/blatt7.rkt
@@ -1,3 +1,39 @@
 #lang racket
 
+; Allgemein rekursiv
+
+(define (zaehlenA zahl liste)
+  
+  (cond
+    [(empty? liste) 0]
+    [[equal? (car liste) zahl] (add1 (zaehlenA zahl (cdr liste)))]
+    [else (zaehlenA zahl (cdr liste))] 
+))  
+
+; Endrekursiv
+
+(define (zaehlenB zahl liste [anzahl 0])
+  
+  (cond
+    [(empty? liste) anzahl]
+    [else (+ (test zahl liste) anzahl)
+          (zaehlenA zahl (cdr liste))]))
+
+
+(define (test unsere_Zahl unsere_Liste)
+  (cond
+    [[equal? (car unsere_Liste) unsere_Zahl]  1]
+    [else 0]))
+
+
+; Mit Funktion höherer Ordnung
+
+(define (zaehlenC zahl1 liste)
+  (count positive? (map (lambda (unser_listenelement)
+
+         (cond
+    [[equal? unser_listenelement zahl1]  1]
+    [else 0]))
+  
+         liste)))
 
