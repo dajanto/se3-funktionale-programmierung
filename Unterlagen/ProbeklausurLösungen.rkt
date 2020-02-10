27e7f498b3e5e6c2ae7a0bd66d71ebc918b6ffae
Jan-Thomas Riemenschneider <dajanto.1337@hotmail.de>
2020-02-10 16:52:57 +0100

    Probeklausur Start

---
+++ Unterlagen/ProbeklausurLösungen.rkt
@@ -0,0 +1,29 @@
+#lang racket
+
+; Probeklausur
+
+; 1.
+
+; a)
+; Ein normaler arithmetischer Ausdruck in Präfix-Notation.
+
+; b)
+; Derselbe arithmetische Ausdruck als Symbol mit quote definiert.
+
+; c)
+; car gibt den ersten Wert einer Liste zurück, Antwort ist also auto.
+
+; d)
+; cdr gibt den Rest einer Liste zurück, schneidet also den Head ab und das Result ist demnach bus.
+
+; e)
+; Es wird sqrt auf jedes Element der übergebenden Liste angewendet, Result ist also '(1 3 2 5)
+
+; f)
+; Mit Hilfe von curry lassen sich Operationsargumente so lange akkumulieren, bis alle Argumente beisammen sind und
+; die Procedure angewendet werden kann. Dann kommt auch ein tatsächliches Ergebnis heraus und keine Procedure.
+; In diesem Fall handelt es sich um eine binäre Operation, das heißt, es wird keine Procedure wiedergegeben,
+; sondern es kommt ein Ergebnis heraus. Ergebnis ist false, da (< 6 2) nicht wahr ist.
+
+; g)
+
